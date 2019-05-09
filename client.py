8d1f2b4aff2c5a7cffe52f4e348ece767dde391a
Hyensoo Choi <hyensoochoi@gmail.com>
2019-05-09 19:27:45 +0900

    그냥 장식입니다

--- client.py
+++ client.py
@@ -9,8 +9,11 @@ key = "lemon-tea"
 
 def send(sock):
     while True:
-        sendData = encrypt(key, input())
-        sock.send(sendData)
+        try:
+            sendData = encrypt(key, input())
+            sock.send(sendData)
+        except:
+            continue
 
 
 def receive(sock):
@@ -25,7 +28,7 @@ def receive(sock):
 port = int(sys.argv[1])
 
 clientSock = socket(AF_INET, SOCK_STREAM)
-clientSock.connect(('127.0.0.1', port))
+clientSock.connect(('45.76.54.218', port))
 
 user_id = str(input("ID: "))
 clientSock.send(user_id.encode('utf-8'))
@@ -33,26 +36,37 @@ clientSock.send(user_id.encode('utf-8'))
 # user_list = clientSock.recv(1024).decode('utf-8')
 # print("USER LIST: {}".format(user_list))
 
-opp_id = str(input("연결 대상자의 ID를 입력하십시오: "))
+opp_id = str(input("연결 대상자의 ID를 입력하십시오(교신을 대기하려면 공백 그대로 입력): "))
 
 if opp_id:
     print(key)
     clientSock.send(opp_id.encode('utf-8'))
 
+    print("교신 대상과 COMM 터널 수립 완료. QKD 양자 키 분배를 시작합니다.")
     key = str(alice.key())
+    time.sleep(1)
+    print("편광 필터 배열, A.L.I.C.E KEY 생성을 완료했습니다. 큐비트 전송 시퀀스에 돌입합니다.")
+    time.sleep(1)
+    print("메인 서버에 피클링된 큐비트와 회로를 전송 중입니다. 기다려 주십시오...")
+    time.sleep(1)
     with open('laser.alice', 'rb') as file:
         clientSock.send(file.read())
 else:
     clientSock.send("~".encode('utf-8'))
     print("교신 대상 ID 공백. 교신 대기.")
     opp_id = clientSock.recv(1024).decode('utf-8')
-    print(opp_id)
+    print("%s로부터의 교신 요청입니다. 수락합니다."%opp_id)
+    time.sleep(1)
+    print("%s로부터 B.O.B KEY, 큐비트, 편광 필터와 회로를 수신합니다..."%opp_id)
+    time.sleep(1)
     laser = clientSock.recv(2048)
     with open('laser.bob', 'wb') as file:
         file.write(laser)
     key = str(bob.key())
 
-print("KEEEEYYYYYEYYEYYYY: ", key)
+
+print("QKD 완료. 양자 채널이 구성되었습니다. LINK START!")
+print("Final AES Encryption Key: ", key)
 
 
 sender = threading.Thread(target=send, args=(clientSock,))
